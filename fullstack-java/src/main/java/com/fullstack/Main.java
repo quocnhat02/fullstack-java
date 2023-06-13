92927f34fddbcc06311515371ef0c3fec3c53781
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-06-13 19:47:46 +0700

    learn spring boot video 42

--- fullstack-java/src/main/java/com/fullstack/Main.java
+++ fullstack-java/src/main/java/com/fullstack/Main.java
@@ -1,14 +1,12 @@
 package com.fullstack;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Objects;
 
 @SpringBootApplication
 @RestController
@@ -17,45 +15,74 @@ public class Main {
         SpringApplication.run(Main.class, args);
     }
 
-    @GetMapping("/")
-    public GreetResponse greet() {
-        GreetResponse response = new GreetResponse(
-                "Hello",
-                List.of("Java", "Golang", "Javascript"),
-                new Person("Nhat", 21, 30000));
-        return response;
+    class Customer {
+        private Integer id;
+        private String name;
+        private String email;
+        private Integer age;
+
+        public Customer() {}
+
+        public Customer(Integer id, String name, String email, Integer age) {
+            this.id = id;
+            this.name = name;
+            this.email = email;
+            this.age = age;
+        }
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public void setAge(Integer age) {
+            this.age = age;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Customer customer = (Customer) o;
+            return Objects.equals(id, customer.id) && Objects.equals(name, customer.name) && Objects.equals(email, customer.email) && Objects.equals(age, customer.age);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, name, email, age);
+        }
+
+        @Override
+        public String toString() {
+            return "Customer{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", email='" + email + '\'' +
+                    ", age=" + age +
+                    '}';
+        }
     }
-
-    record Person(String name, int age, double savings) {}
-
-    record GreetResponse(String greet, List<String> favProgrammingLanguages, Person person) {}
-
-//    class GreetResponse {
-//        private final String greet;
-//
-//        GreetResponse(String greet) {
-//            this.greet = greet;
-//        }
-//
-//        public String getGreet() {
-//            return greet;
-//        }
-//
-//        @Override
-//        public String toString() {
-//            return "GreetResponse{" +
-//                    "greet='" + greet + "\'" +
-//                    "}";
-//        }
-//
-//        @Override
-//        public int hashCode() {
-//            return super.hashCode();
-//        }
-//
-//        @Override
-//        public boolean equals(Object obj) {
-//            return super.equals(obj);
-//        }
-//    }
 }
