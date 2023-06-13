b3e38b2c04fed9701bdf6e56e2db298fb153e9b5
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-06-13 21:23:49 +0700

    learn spring boot video 48

--- fullstack-java/src/main/java/com/fullstack/Main.java
+++ fullstack-java/src/main/java/com/fullstack/Main.java
@@ -1,5 +1,8 @@
 package com.fullstack;
 
+import com.fullstack.customer.CustomerController;
+import com.fullstack.customer.CustomerDataAccessService;
+import com.fullstack.customer.CustomerService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +10,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Main {
 
     public static void main(String[] args) {
+        /*
+        CustomerService customerService =
+                new CustomerService(new CustomerDataAccessService());
+        CustomerController customerController =
+                new CustomerController(customerService);
+        */
+
         SpringApplication.run(Main.class, args);
     }
 
--- fullstack-java/src/main/java/com/fullstack/customer/CustomerService.java
+++ fullstack-java/src/main/java/com/fullstack/customer/CustomerService.java
@@ -1,8 +1,11 @@
 package com.fullstack.customer;
 
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class CustomerService {
 
     private final CustomerDao customerDao;
