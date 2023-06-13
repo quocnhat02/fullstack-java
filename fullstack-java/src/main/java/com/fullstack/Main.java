a8df6a98c4c71876b82471276a76926c86096f18
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-06-13 17:21:35 +0700

    learn spring boot video 25

--- fullstack-java/src/main/java/com/fullstack/Main.java
+++ fullstack-java/src/main/java/com/fullstack/Main.java
@@ -20,5 +20,34 @@ public class Main {
         return new GreetResponse("Hello");
     }
 
-    record GreetResponse(String greet) {}
+//    record GreetResponse(String greet) {}
+
+    class GreetResponse {
+        private final String greet;
+
+        GreetResponse(String greet) {
+            this.greet = greet;
+        }
+
+        public String getGreet() {
+            return greet;
+        }
+
+        @Override
+        public String toString() {
+            return "GreetResponse{" +
+                    "greet='" + greet + "\'" +
+                    "}";
+        }
+
+        @Override
+        public int hashCode() {
+            return super.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return super.equals(obj);
+        }
+    }
 }
