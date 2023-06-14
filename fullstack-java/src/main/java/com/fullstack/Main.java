1ad4ae39076e22faeedbbed1b3e883054ea2ec1c
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-06-14 13:26:42 +0700

    fullstack video 50

--- fullstack-java/src/main/java/com/fullstack/Main.java
+++ fullstack-java/src/main/java/com/fullstack/Main.java
@@ -5,6 +5,7 @@ import com.fullstack.customer.CustomerDataAccessService;
 import com.fullstack.customer.CustomerService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Main {
@@ -17,7 +18,13 @@ public class Main {
                 new CustomerController(customerService);
         */
 
-        SpringApplication.run(Main.class, args);
+        ConfigurableApplicationContext applicationContext =
+                SpringApplication.run(Main.class, args);
+
+        String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            System.out.println(beanDefinitionName);
+        }
     }
 
 }
